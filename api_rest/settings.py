16b85a3444b57dd6f1bdb5c026dc58c3c9832234
Alan <alanorejon2001@gmail.com>
2023-05-25 18:07:06 -0300

    testing new branch

--- api_rest/settings.py
+++ api_rest/settings.py
@@ -118,8 +118,12 @@ WSGI_APPLICATION = 'api_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
--- peliculas/api/views.py
+++ peliculas/api/views.py
@@ -94,7 +94,6 @@ class CharacterViewSet(ModelViewSet):
             * en caso de no recibir una query, devuelve un listado de todos los personajes
         """
         param = request.query_params
-
         if param:
 
             if param.get('name'):
@@ -177,7 +176,7 @@ class MovieViewSet(ModelViewSet):
 
         else:
             movies = self.get_serializer(self.get_queryset(), many=True)
-            return Response(movies.data, status=status.HTTP_200_OK)
+            return {list(movies.data)}
 
     def retrieve(self, request, *args, **kwargs):
         """
